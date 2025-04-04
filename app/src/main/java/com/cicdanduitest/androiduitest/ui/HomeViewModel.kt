6725025c117ebe551005fced2e0f81c7ca1c5ae2
Akash G <dragon.abhi8@gmail.com>
2025-04-05 04:34:32 +0530

    First Commit

--- app/src/main/java/com/cicdanduitest/androiduitest/ui/HomeViewModel.kt
+++ app/src/main/java/com/cicdanduitest/androiduitest/ui/HomeViewModel.kt
@@ -3,4 +3,7 @@ package com.cicdanduitest.androiduitest.ui
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+
+    fun add(a: Int, b: Int): Int = a + b
+    fun subtract(a: Int, b: Int): Int = a - b
 }
