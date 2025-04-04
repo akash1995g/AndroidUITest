70c05a4b5c785a6fc21ae5034056e33c65bc6c24
Akash G <dragon.abhi8@gmail.com>
2025-04-05 04:00:38 +0530

    First Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+import java.io.ByteArrayOutputStream
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -59,4 +61,89 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+spotless {
+    java {
+        target("**/*.java")
+        googleJavaFormat().aosp()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+    kotlin {
+        target("**/*.kt")
+        trimTrailingWhitespace()
+        ktlint()
+        indentWithSpaces()
+        endWithNewline()
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", getLocalProperty("sonar.key", "config.properties"))
+        property("sonar.projectName", getLocalProperty("sonar.name", "config.properties"))
+        property("sonar.host.url", getLocalProperty("sonar.url", "config.properties"))
+        property("sonar.token", getLocalProperty("sonar.token", "config.properties"))
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            "${project.buildDir}/reports/kover/xml/report.xml"
+        )
+        property("sonar.coverage.exclusions", " **/*Fragment.kt, **/*Activity*")
+        // Fetch the current Git branch name
+        val branchName = getCurrentGitBranch()
+        property("sonar.branch.name", branchName)
+    }
+}
+
+fun Project.getLocalProperty(key: String, file: String = "local.properties"): Any {
+    val properties = Properties()
+    properties.load(project.rootProject.file(file).reader())
+    return properties.getProperty(key)
+}
+
+fun getCurrentGitBranch(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "rev-parse", "--abbrev-ref", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
+}
+
+
+kover {
+    filters {
+        classes {
+            excludes.addAll(listOf("*Fragment*", "*Activity*"))
+        }
+        annotations {
+            excludes.addAll(listOf("*Generated", "*CustomAnnotationToExclude"))
+        }
+    }
+    verify {
+        rule {
+            //            isEnabled = true
+            /* name = "Coverage must be more than 60%"
+             bound {
+                 minValue = 60
+             }*/
+        }
+    }
+
+    htmlReport {
+        onCheck.set(false)
+    }
+    xmlReport {
+        onCheck.set(false)
+    }
+}
+
+task("codeCheck") {
+    group = "verification"
+    description = "Generates the Kover XML report and then runs SonarQube analysis."
+
+    dependsOn(":app:spotlessApply", ":app:koverXmlReport", ":app:sonar")
 }
