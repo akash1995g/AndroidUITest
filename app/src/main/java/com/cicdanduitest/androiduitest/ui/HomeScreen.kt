eec6b1a503398aa85302aff7eec8c55412550720
Akash G <dragon.abhi8@gmail.com>
2025-04-05 04:49:51 +0530

    Spotless

--- app/src/main/java/com/cicdanduitest/androiduitest/ui/HomeScreen.kt
+++ app/src/main/java/com/cicdanduitest/androiduitest/ui/HomeScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 
-
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     val viewModel: HomeViewModel = viewModel()
@@ -14,5 +13,4 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 @Preview
 @Composable
 private fun HomeScreenPreview(modifier: Modifier = Modifier) {
-
 }
