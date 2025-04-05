0a671d9a6adb4fd8f52f654e95da44f9e17890b5
Akash G <dragon.abhi8@gmail.com>
2025-04-05 16:52:04 +0530

    Add colors to sonar exception

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,7 +104,7 @@ sonar {
         property("sonar.coverage.exclusions", "**/*Fragment.kt, **/*Activity*,**/ui/theme/*")
         // Fetch the current Git branch name
         val branchName = getCurrentGitBranch()
-        property("sonar.branch.name", branchName)
+        property("sonar.branch.name", System.getenv("CURRENT_BRANCH") ?: branchName)
     }
 }
 
