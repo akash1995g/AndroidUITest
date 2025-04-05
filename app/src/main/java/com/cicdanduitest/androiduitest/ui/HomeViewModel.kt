64a6669f5fcda973d4eaaabc9fc51e48e740e0dc
Akash G <dragon.abhi8@gmail.com>
2025-04-05 06:31:57 +0530

    With Parallel Stage

--- app/src/main/java/com/cicdanduitest/androiduitest/ui/HomeViewModel.kt
+++ app/src/main/java/com/cicdanduitest/androiduitest/ui/HomeViewModel.kt
@@ -7,4 +7,5 @@ class HomeViewModel : ViewModel() {
     fun add(a: Int, b: Int): Int = a + b
     fun subtract(a: Int, b: Int): Int = a - b
     fun div(a: Int, b: Int): Int = a / b
+    fun multi(a: Int, b: Int): Int = a * b
 }
